6af2ff2d43862e96490249ae4a05283eafe3b598
Mason Arditi <masonarditi@berkeley.edu>
2025-04-22 03:12:58 -0700

    chat input, 21st

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-   <div>
-    <h1>Hello World</h1>
-   </div>
-  );
+import {
+	ChatInput,
+	ChatInputSubmit,
+	ChatInputTextArea,
+} from "@/components/ui/chat-input";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function ChatInputDemo() {
+	const [value, setValue] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleSubmit = () => {
+		setIsLoading(true);
+		setTimeout(() => {
+			toast(value);
+			setIsLoading(false);
+		}, 1000);
+	};
+
+	return (
+		<div className="w-full max-w-[400px] h-full mx-auto mt-20 flex items-center justify-center">
+			<ChatInput
+				variant="default"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				onSubmit={handleSubmit}
+				loading={isLoading}
+				onStop={() => setIsLoading(false)}
+			>
+				<ChatInputTextArea placeholder="Type a message..." />
+				<ChatInputSubmit />
+			</ChatInput>
+		</div>
+	);
 }
+
+export { ChatInputDemo }
